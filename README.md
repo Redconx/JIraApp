2fadf98692171c600da2c08327be46b12777f5e6
redconx <ajaynaugain907@gmail.com>
2023-03-30 22:24:04 +0530

    second commit

--- README.md
+++ README.md
@@ -5,9 +5,12 @@ This is a jira issue management app. We used jira's developers Rest API fro this
 RestApi link : https://developer.atlassian.com/server/jira/platform/rest-apis/
 In this App you can see all the neccesary information about the issue and also can change the status from TO-DO to Done with comment.
 
+# Data Storage
+Now data is being stored on supabase server. Also you can see data on youyr UI.
+
 # How to Install and Run the Project
 
-- Download the code from the Github to your machine.
+- Download the code from the Github repository to your machine.
 - Then in source folder open terminal and type :{npm install} to install all dependencies in your       machine.
 - Open the project in VS Code
 - now open terminal in VS Code and type : {node server.js} to run server file.
--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-var axios=require('axios')
+var axios = require("axios");
 var express = require("express");
 var app = express();
 app.use(express.json());
@@ -8,7 +8,7 @@ app.use(function (req, res, next) {
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
   );
-    res.header(
+  res.header(
     "Access-Control-Allow-Methods",
     "GET, POST, OPTIONS, PUT, PATCH, DELETE, HEAD"
   );
@@ -17,32 +17,114 @@ app.use(function (req, res, next) {
 const port = 2410;
 app.listen(port, () => console.log(`Node app listening on port ${port}!`));
 
+const { Client } = require("pg");
+const client = new Client({
+  user: "postgres",
+  password: "Ajay1#vinay",
+  database: "postgres",
+  port: 5432,
+  host: "db.bvkchvpqwcbvyebrefej.supabase.co",
+});
+client.connect(function (res, error) {
+  console.log("connected !!!");
+});
+const URL1 = "https://ajayjiraapp.atlassian.net/rest/api/2/search";
 
-const URL1='https://ajayjiraapp.atlassian.net/rest/api/2/search'
+pushDataToDb();
 
-app.get("/issues", async function(req,res){
-    console.log("hi");
-    try {
-        let response = await axios.get( URL1 , {headers:{Authorization:'Basic YWpuYXVnYWluOTA3QGdtYWlsLmNvbTpBVEFUVDN4RmZHRjBEUHBEUkFEdFZSdWVNWlN1VFAxUlRFbjJVb3R6VGk1eF9Bb1c2UUZVQW5SbDhrR3BLdDhwOU5nUzYtME9uT2t6R1hFVnltXzNIcFRzdE9VTklJdWtpdGppVDlkS3M1TTMyeUdnVmM1NXQxTU9FMU44VzhLOEhINkhVcm9TcUdqMTNCQU00TTNjNFFUdjZrX0dHalVoT3NUNmJjcFRJT2R5dk5RZklOR2xIY1k9QzQ2MEQ4RTM'}})
-        res.send(response.data.issues)
-    } catch (error) {
-        console.log(error.message,'error')   
-    }
-})
+async function pushDataToDb() {
+  try {
+    let response = await axios.get(URL1, {
+      headers: {
+        Authorization:
+          "Basic YWpuYXVnYWluOTA3QGdtYWlsLmNvbTpBVEFUVDN4RmZHRjBEUHBEUkFEdFZSdWVNWlN1VFAxUlRFbjJVb3R6VGk1eF9Bb1c2UUZVQW5SbDhrR3BLdDhwOU5nUzYtME9uT2t6R1hFVnltXzNIcFRzdE9VTklJdWtpdGppVDlkS3M1TTMyeUdnVmM1NXQxTU9FMU44VzhLOEhINkhVcm9TcUdqMTNCQU00TTNjNFFUdjZrX0dHalVoT3NUNmJjcFRJT2R5dk5RZklOR2xIY1k9QzQ2MEQ4RTM",
+      },
+    });
+    let dataForDb = response.data.issues.map((ele) => ({
+      number: ele.key,
+      name: ele.fields.summary,
+      description: ele.fields.description,
+      reporter: ele.fields.reporter.displayName,
+      status: ele.fields.status.name,
+      duedate: ele.fields.duedate,
+    }));
 
-app.post("/changeStatus",async function(req,res){
-    const {comment,key}=req.body
-    console.log(req.body)
-    console.log(comment,key)
-    const obj1={transition: {id: "31"}}
-    const obj2={body:comment}
-    try {
-        let resp1= await axios.post(`https://ajayjiraapp.atlassian.net/rest/api/3/issue/${key}/transitions`,obj1,{headers:{Authorization:'Basic YWpuYXVnYWluOTA3QGdtYWlsLmNvbTpBVEFUVDN4RmZHRjBEUHBEUkFEdFZSdWVNWlN1VFAxUlRFbjJVb3R6VGk1eF9Bb1c2UUZVQW5SbDhrR3BLdDhwOU5nUzYtME9uT2t6R1hFVnltXzNIcFRzdE9VTklJdWtpdGppVDlkS3M1TTMyeUdnVmM1NXQxTU9FMU44VzhLOEhINkhVcm9TcUdqMTNCQU00TTNjNFFUdjZrX0dHalVoT3NUNmJjcFRJT2R5dk5RZklOR2xIY1k9QzQ2MEQ4RTM'}})
-        let resp2= await axios.post(`https://ajayjiraapp.atlassian.net/rest/api/2/issue/${key}/comment`,obj2,{headers:{Authorization:'Basic YWpuYXVnYWluOTA3QGdtYWlsLmNvbTpBVEFUVDN4RmZHRjBEUHBEUkFEdFZSdWVNWlN1VFAxUlRFbjJVb3R6VGk1eF9Bb1c2UUZVQW5SbDhrR3BLdDhwOU5nUzYtME9uT2t6R1hFVnltXzNIcFRzdE9VTklJdWtpdGppVDlkS3M1TTMyeUdnVmM1NXQxTU9FMU44VzhLOEhINkhVcm9TcUdqMTNCQU00TTNjNFFUdjZrX0dHalVoT3NUNmJjcFRJT2R5dk5RZklOR2xIY1k9QzQ2MEQ4RTM'}})
-        res.send('status changed succesfully')
-    } catch (error) {
-        console.log(error.message,'error')
-        res.send(error.message)
-    }
-    
-})
+    let sql = "truncate table issues";
+    client.query(sql, function (err, result) {
+      if (err) console.log(err);
+      else {
+        console.log("table emptied success");
+        resetData();
+      }
+    });
+
+    resetData = () => {
+      let issueArray = dataForDb.map((ele) => [
+        ele.number,
+        ele.name,
+        ele.description,
+        ele.reporter,
+        ele.status,
+        ele.duedate,
+      ]);
+      console.log(issueArray);
+      issueArray.map((ele) => {
+        console.log(ele);
+        let sql1 = "INSERT INTO issues VALUES ($1,$2,$3,$4,$5,$6)";
+        client.query(sql1, ele, function (err, result) {});
+      });
+    };
+  } catch (error) {
+    console.log(error.message, "error");
+  }
+}
+
+app.get("/issues", async function (req, res) {
+  console.log("hi");
+  try {
+    let response = await axios.get(URL1, {
+      headers: {
+        Authorization:
+          "Basic YWpuYXVnYWluOTA3QGdtYWlsLmNvbTpBVEFUVDN4RmZHRjBEUHBEUkFEdFZSdWVNWlN1VFAxUlRFbjJVb3R6VGk1eF9Bb1c2UUZVQW5SbDhrR3BLdDhwOU5nUzYtME9uT2t6R1hFVnltXzNIcFRzdE9VTklJdWtpdGppVDlkS3M1TTMyeUdnVmM1NXQxTU9FMU44VzhLOEhINkhVcm9TcUdqMTNCQU00TTNjNFFUdjZrX0dHalVoT3NUNmJjcFRJT2R5dk5RZklOR2xIY1k9QzQ2MEQ4RTM",
+      },
+    });
+    pushDataToDb();
+    res.send(response.data.issues);
+  } catch (error) {
+    console.log(error.message, "error");
+  }
+});
+
+app.post("/changeStatus", async function (req, res) {
+  const { comment, key } = req.body;
+  console.log(req.body);
+  console.log(comment, key);
+  const obj1 = { transition: { id: "31" } };
+  const obj2 = { body: comment };
+  try {
+    let resp1 = await axios.post(
+      `https://ajayjiraapp.atlassian.net/rest/api/3/issue/${key}/transitions`,
+      obj1,
+      {
+        headers: {
+          Authorization:
+            "Basic YWpuYXVnYWluOTA3QGdtYWlsLmNvbTpBVEFUVDN4RmZHRjBEUHBEUkFEdFZSdWVNWlN1VFAxUlRFbjJVb3R6VGk1eF9Bb1c2UUZVQW5SbDhrR3BLdDhwOU5nUzYtME9uT2t6R1hFVnltXzNIcFRzdE9VTklJdWtpdGppVDlkS3M1TTMyeUdnVmM1NXQxTU9FMU44VzhLOEhINkhVcm9TcUdqMTNCQU00TTNjNFFUdjZrX0dHalVoT3NUNmJjcFRJT2R5dk5RZklOR2xIY1k9QzQ2MEQ4RTM",
+        },
+      }
+    );
+    let resp2 = await axios.post(
+      `https://ajayjiraapp.atlassian.net/rest/api/2/issue/${key}/comment`,
+      obj2,
+      {
+        headers: {
+          Authorization:
+            "Basic YWpuYXVnYWluOTA3QGdtYWlsLmNvbTpBVEFUVDN4RmZHRjBEUHBEUkFEdFZSdWVNWlN1VFAxUlRFbjJVb3R6VGk1eF9Bb1c2UUZVQW5SbDhrR3BLdDhwOU5nUzYtME9uT2t6R1hFVnltXzNIcFRzdE9VTklJdWtpdGppVDlkS3M1TTMyeUdnVmM1NXQxTU9FMU44VzhLOEhINkhVcm9TcUdqMTNCQU00TTNjNFFUdjZrX0dHalVoT3NUNmJjcFRJT2R5dk5RZklOR2xIY1k9QzQ2MEQ4RTM",
+        },
+      }
+    );
+    res.send("status changed succesfully");
+  } catch (error) {
+    console.log(error.message, "error");
+    res.send(error.message);
+  }
+});
